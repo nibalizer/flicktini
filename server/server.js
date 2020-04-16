439899524deb85b9e27347cd36ab1ae1eb08a8c3
Spencer Krum <nibz@spencerkrum.com>
2020-04-16 06:19:52 +0000

    Initial commit
    * Started with IBM express starter repo
    * Added the tiniest bit of csgo state parsing

--- server/server.js
+++ server/server.js
@@ -8,6 +8,8 @@ const swaggerRoutes = require('./routes/swagger-route');
 
 const app = express();
 
+let temp_state = { round: 0 };
+
 // enable parsing of http request body
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -16,13 +18,45 @@ app.use(bodyParser.json());
 app.use('/health', healthRoutes);
 app.use('/swagger', swaggerRoutes);
 
+function parsePhase(state, prevState) {
+  let changed = false;
+  if ('round' in state) {
+    if (process.env.DEBUG) {
+      console.log('game state is: ' + state.round.phase);
+    }
+    if (prevState.round.phase !== state.round.phase) {
+      changed = true;
+      // round just ended
+    }
+    if (state.round.phase === 'over' && changed) {
+      return true;
+    } else {
+      return false;
+    }
+  } else {
+    return false;
+  }
+}
+
+app.post('/', (req, res) => {
+  let state = req.body;
+  if (process.env.DEBUG) {
+    console.log(req.body);
+  }
+  if (parsePhase(state, temp_state)) {
+    console.log('New Round');
+  }
+
+  temp_state = state;
+});
+
 // default path to serve up index.html (single page application)
 app.all('', (req, res) => {
   res.status(200).sendFile(path.join(__dirname, '../public', 'index.html'));
 });
 
 // start node server
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4500;
 app.listen(port, () => {
   console.log(`App UI available http://localhost:${port}`);
   console.log(`Swagger UI available http://localhost:${port}/swagger/api-docs`);
